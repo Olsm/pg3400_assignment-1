1ede40b3d53cbdeebf60080d21415c8814de2983
Olsm <OlavOlsm@gmail.com>
2016-10-30 15:00:55 +0100

    read file, save lines to a char array and count them

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello World");
+    char name[48];
+    char names[2048][sizeof(name)];
+    int count = 0;
+    FILE * file;
+
+    file = fopen("txt/ships.txt", "r");
+    if (file == NULL){
+        perror("Could not open file");
+        exit(1);
+    }
+
+    while (fgets(name, sizeof(name), file)) {
+        printf("%s", name);
+        strcpy(names[count], name);
+        count++;
+    }
+
+    fclose(file);
+    exit(0);
 }
