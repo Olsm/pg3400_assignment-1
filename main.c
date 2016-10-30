05d28fb775ccaf77c4d2259ed67c3639d84607ed
Olsm <OlavOlsm@gmail.com>
2016-10-30 19:14:40 +0100

    Sort array of names with bubble sort and print result

--- main.c
+++ main.c
@@ -9,18 +9,41 @@ int main()
     int count = 0;
     FILE * file;
 
+    // Try and open the file for reading, or exit with error
+
     file = fopen("txt/ships.txt", "r");
     if (file == NULL){
         perror("Could not open file");
         exit(1);
     }
 
+    // Read each line and put them in an array of names
+
     while (fgets(name, sizeof(name), file)) {
-        printf("%s", name);
         strcpy(names[count], name);
         count++;
     }
 
     fclose(file);
-    exit(0);
+
+    // Sort the names alphabetically
+
+    int i, j, k;
+    char temp[sizeof(name)];
+
+    for (i = 0; i < count - 1 ; i++) {
+        for (j = i + 1; j < count; j++) {
+            if (strcmp(names[i], names[j]) > 0) {
+                strcpy(temp, names[i]);
+                strcpy(names[i], names[j]);
+                strcpy(names[j], temp);
+            }
+        }
+    }
+
+    // Print the sorted names
+
+    for (k = 0; k < count; k++) {
+        printf("%s", names[k]);
+    }
 }
